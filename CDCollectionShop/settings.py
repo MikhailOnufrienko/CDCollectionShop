3569bf977dcc407cf94c2912f72a7a543368663d
MikhailOnufrienko <ghast13@yandex.ru>
2022-10-05 13:09:34 +0500

    Added account app; added account.models and account.forms.

--- CDCollectionShop/settings.py
+++ CDCollectionShop/settings.py
@@ -22,7 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'main',
-    'basket'
+    'basket',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -98,3 +99,7 @@ STATIC_ROOT = '/static/'
 STATICFILES_DIRS = [(BASE_DIR / 'static'),]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'account.UserBase'
+LOGIN_REDIRECT_URL = '/account/dashboard'
+LOGIN_URL = '/account/login/'
