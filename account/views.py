3f8a6e8d7abdd323ed7ff41a08ac1481af5d032d
MikhailOnufrienko <ghast13@yandex.ru>
2022-10-06 14:16:14 +0500

    Added links to dashboard.

--- account/views.py
+++ account/views.py
@@ -7,7 +7,7 @@ from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
-# from orders.views import user_orders
+from order.views import user_orders
 
 from .forms import RegistrationForm, UserEditForm
 from .models import UserBase
